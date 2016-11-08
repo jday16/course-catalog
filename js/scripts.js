8ef76a52fc482d8b1fdca5b8329b28ae9df40780
jday16 <jday16@vcd-ch204-23.eastern.ewu.edu>
2016-11-08 14:35:55 -0800

    version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 // YOUR SCRIPTS
+//Grid View//
+$('#grid').click(function () {
+    //Remove ONLY list class//
+    $('.cd-gallery').removeClass('list');
+    //Add ONLY grid class//
+    $('.cd-gallery').addClass('grid');
+});
+
+//List View//
+$('#list').click(function () {
+    //Remove ONLY list class//
+    $('.cd-gallery').removeClass('grid');
+    //Add ONLY grid class//
+    $('.cd-gallery').addClass('list');
+});

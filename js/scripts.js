d9c508afc77c1df0fa369bf266da8e187b8c12b2
jday16 <janessaday@eagles.ewu.edu>
2016-11-13 15:45:49 -0800

    Added filters

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-// YOUR SCRIPTS
-//Grid View//
-$('#grid').click(function () {
-    //Remove ONLY list class//
-    $('.cd-gallery').removeClass('list');
-    //Add ONLY grid class//
-    $('.cd-gallery').addClass('grid');
+// GRID VIEW 
+
+$('#grid').click( function(){
+    
+    // Remove ONLY 'list' class
+    
+$('.cd-gallery').removeClass('list');   
+    
+    // add ONLY 'grid' class
+$('.cd-gallery').addClass('grid');
+
+
 });
 
-//List View//
-$('#list').click(function () {
-    //Remove ONLY list class//
-    $('.cd-gallery').removeClass('grid');
-    //Add ONLY grid class//
-    $('.cd-gallery').addClass('list');
+// LIST VIEW 
+
+$('#list').click( function(){
+    
+    // Remove ONLY 'list' class
+    
+$('.cd-gallery').removeClass('grid');   
+    
+    // add ONLY 'grid' class
+$('.cd-gallery').addClass('list');
+    
 });
+
+
+//clear filter
+
+$(document).on('click', '#clear-filter', function(){       
+    $('input[data-type="search"]').val('');
+    $('input[data-type="search"]').trigger("keyup");
+});
+
+$('#form')[0].reset(); 
+$('#form').get(0).reset();
